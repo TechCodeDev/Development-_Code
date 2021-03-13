2cf4498d5e7802a06c8391edea84af1c6d2b7260
Puneeth N <42162624+TechCodeDev@users.noreply.github.com>
2021-03-13 18:19:58 +0530

    Update Select.sql

--- Sql/Select.sql
+++ Sql/Select.sql
@@ -1,1 +1,7 @@
+SELECT DISTINCT D.GroupName FROM HumanResources.Department D
 
+SELECT DISTINCT D.* FROM HumanResources.Department D
+
+SELECT DISTINCT D.Name, D.GroupName FROM HumanResources.Department D
+
+SELECT DISTINCT  D.GroupName, D.Name FROM HumanResources.Department D
